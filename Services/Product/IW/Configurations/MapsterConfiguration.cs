c0d6b1b9134ef299aa43cec0b275848415041445
haifam <haiphamtrung62@gmail.com>
2023-10-22 21:40:02 +0700

    Add comment to mapster configuration file

--- Services/Product/IW/Configurations/MapsterConfiguration.cs
+++ Services/Product/IW/Configurations/MapsterConfiguration.cs
@@ -5,8 +5,15 @@ using Mapster;
 
 namespace IW.Configurations
 {
+    /// <summary>
+    /// Configuration file for global type adapter. Remember to load it in when unit Testing
+    /// </summary>
     public class MapsterConfiguration : IRegister
     {
+        /// <summary>
+        /// Register your TypeAdapterConfig for global using.
+        /// </summary>
+        /// <param name="config"></param>
         public void Register(TypeAdapterConfig config)
         {
             config
