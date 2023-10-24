79cf7528ff2af90148ae4dc46a6e329b52e23759
Hai Pham <79838488+PhamTrungHai@users.noreply.github.com>
2023-10-24 10:38:07 +0700

    add CORs

--- Services/Cart/IW/Program.cs
+++ Services/Cart/IW/Program.cs
@@ -16,6 +16,11 @@ if (app.Environment.IsDevelopment())
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseCors(builder => builder
+    .WithOrigins("https://studio.apollographql.com", "http://localhost:3000", "http://localhost:8080") // Add your allowed origins here
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials());
 //app.UseMiddleware<ErrorHandlerMiddleware>();
 
 app.MapGraphQL();
