6e9ae43a2e3b4cafe0924fcd7e4a6d9c09f8ae1e
haifam <haiphamtrung62@gmail.com>
2023-11-20 20:50:39 +0700

    add mapster and rabbitmq

---
+++ Services/User/IW/Configurations/MapsterConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using IW.Models;
+using IW.Models.DTOs;
+using IW.Models.DTOs.User;
+using IW.Models.Entities;
+using Mapster;
+
+namespace IW.Configurations
+{
+    /// <summary>
+    /// Configuration file for global type adapter.Remember to load it in when unit Testing
+    /// </summary>
+    public class MapsterConfiguration : IRegister
+    {
+        /// <summary>
+        /// Register your TypeAdapterConfig for global using.
+        /// </summary>
+        /// <param name = "config" ></ param >
+        public void Register(TypeAdapterConfig config)
+    {
+        //config
+        //    .NewConfig<OrderItem, ItemDto>()
+        //    .Ignore(dst => dst.Order);
+        //config
+        //    .NewConfig<Order, OrderCreatedMessage>()
+        //    .Map(dst => dst.Items, src => src.Items);
+        //config
+        //    .Default
+        //    .PreserveReference(true);
+        //config
+        //    .NewConfig<ItemDto, OrderItem>()
+        //    .Map(dst => dst.Subtotal, src => src.Price * src.Quantity);
+        //config
+        //    .NewConfig<ItemDto, OrderItem>()
+        //    .Map(dst => dst.Subtotal, src => src.Price * src.Quantity);
+        //config
+        //    .NewConfig<CreateItem, OrderItem>()
+        //    .Map(dst => dst.Subtotal, src => src.Price * src.Quantity);
+        config
+            .NewConfig<User, UserDto>()
+            .Map(dst => dst.Address, src => src.Addresses!=null ? src.Addresses : null);
+    }
+}
+}
---
+++ Services/User/IW/Configurations/RabbitMqOptionSetup.cs
@@ -0,0 +1,22 @@
+﻿using IW.Authentication;
+using Microsoft.Extensions.Options;
+
+namespace IW.Configurations
+{
+    public class RabbitMqOptionSetup : IConfigureOptions<RabbitMqOptions>
+    {
+        private const string SectionName = "RabbitMq";
+
+        private readonly IConfiguration _configuration;
+
+        public RabbitMqOptionSetup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(RabbitMqOptions options)
+        {
+            _configuration.GetSection(SectionName).Bind(options);
+        }
+    }
+}
