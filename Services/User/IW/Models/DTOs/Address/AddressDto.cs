48b7f265b6d5d2dd2f36d1b8e99643c755a88518
trthsa <trthsan@gmail.com>
2023-11-26 04:18:10 +0000

    add optional AddressDTO

--- Services/User/IW/Models/DTOs/Address/AddressDto.cs
+++ Services/User/IW/Models/DTOs/Address/AddressDto.cs
@@ -2,14 +2,14 @@
 {
     public class AddressDto
     {
-        public int Id { get; set; }
-        public Guid UserId { get; set; }
-        public bool IsDefault { get; set; }
-        public string Phone { get; set; }
-        public string Name { get; set; }
-        public string Detail { get; set; }
-        public string Ward { get; set; }
-        public string District { get; set; }
-        public string City { get; set; }
+        public int? Id { get; set; }
+        public Guid? UserId { get; set; }
+        public bool? IsDefault { get; set; }
+        public string? Phone { get; set; }
+        public string? Name { get; set; }
+        public string? Detail { get; set; }
+        public string? Ward { get; set; }
+        public string? District { get; set; }
+        public string? City { get; set; }
     }
 }
