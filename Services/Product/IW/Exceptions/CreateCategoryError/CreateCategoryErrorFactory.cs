dab8484747b0eb08acba53dc4afd3a80e0c14efb
TS <trthsan@gmail.com>
2023-12-07 12:55:00 +0700

    add delete for categories mutation

--- Services/Product/IW/Exceptions/CreateCategoryError/CreateCategoryErrorFactory.cs
+++ Services/Product/IW/Exceptions/CreateCategoryError/CreateCategoryErrorFactory.cs
@@ -9,4 +9,12 @@ namespace IW.Exceptions.CreateCategoryError
             return new ValidateCategoryError(ex.Errors);
         }
     }
+    public class DeleteCategoryErrorFactory :
+      IPayloadErrorFactory<ValidateCategoryException, ValidateCategoryError>
+    {
+        public ValidateCategoryError CreateErrorFrom(ValidateCategoryException ex)
+        {
+            return new ValidateCategoryError(ex.Errors);
+        }
+    }
 }
